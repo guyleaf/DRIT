debcddaff0f5b6352374d80272d8a0026bc2b4b8
Leaf Ying <leaf.ying.work@gmail.com>
2024-10-24 15:34:43 +0800

    Renew the usage of sigmoid function

--- src/model.py
+++ src/model.py
@@ -1,6 +1,8 @@
-import networks
 import torch
 import torch.nn as nn
+
+import networks
+
 
 class DRIT(nn.Module):
   def __init__(self, opts):
@@ -261,8 +263,8 @@ class DRIT(nn.Module):
     pred_real = netD.forward(real)
     loss_D = 0
     for it, (out_a, out_b) in enumerate(zip(pred_fake, pred_real)):
-      out_fake = nn.functional.sigmoid(out_a)
-      out_real = nn.functional.sigmoid(out_b)
+      out_fake = torch.sigmoid(out_a)
+      out_real = torch.sigmoid(out_b)
       all0 = torch.zeros_like(out_fake).cuda(self.gpu)
       all1 = torch.ones_like(out_real).cuda(self.gpu)
       ad_fake_loss = nn.functional.binary_cross_entropy(out_fake, all0)
@@ -275,8 +277,8 @@ class DRIT(nn.Module):
     pred_fake = self.disContent.forward(imageA.detach())
     pred_real = self.disContent.forward(imageB.detach())
     for it, (out_a, out_b) in enumerate(zip(pred_fake, pred_real)):
-      out_fake = nn.functional.sigmoid(out_a)
-      out_real = nn.functional.sigmoid(out_b)
+      out_fake = torch.sigmoid(out_a)
+      out_real = torch.sigmoid(out_b)
       all1 = torch.ones((out_real.size(0))).cuda(self.gpu)
       all0 = torch.zeros((out_fake.size(0))).cuda(self.gpu)
       ad_true_loss = nn.functional.binary_cross_entropy(out_real, all1)
@@ -291,15 +293,16 @@ class DRIT(nn.Module):
     self.enc_a_opt.zero_grad()
     self.gen_opt.zero_grad()
     self.backward_EG()
-    self.enc_c_opt.step()
-    self.enc_a_opt.step()
-    self.gen_opt.step()
+    # self.enc_c_opt.step()
+    # self.enc_a_opt.step()
+    # self.gen_opt.step()
 
     # update G, Ec
-    self.enc_c_opt.zero_grad()
-    self.gen_opt.zero_grad()
+    # self.enc_c_opt.zero_grad()
+    # self.gen_opt.zero_grad()
     self.backward_G_alone()
     self.enc_c_opt.step()
+    self.enc_a_opt.step()
     self.gen_opt.step()
 
   def backward_EG(self):
@@ -357,7 +360,7 @@ class DRIT(nn.Module):
   def backward_G_GAN_content(self, data):
     outs = self.disContent.forward(data)
     for out in outs:
-      outputs_fake = nn.functional.sigmoid(out)
+      outputs_fake = torch.sigmoid(out)
       all_half = 0.5*torch.ones((outputs_fake.size(0))).cuda(self.gpu)
       ad_loss = nn.functional.binary_cross_entropy(outputs_fake, all_half)
     return ad_loss
@@ -366,7 +369,7 @@ class DRIT(nn.Module):
     outs_fake = netD.forward(fake)
     loss_G = 0
     for out_a in outs_fake:
-      outputs_fake = nn.functional.sigmoid(out_a)
+      outputs_fake = torch.sigmoid(out_a)
       all_ones = torch.ones_like(outputs_fake).cuda(self.gpu)
       loss_G += nn.functional.binary_cross_entropy(outputs_fake, all_ones)
     return loss_G
